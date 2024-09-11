9e58912869a969a94014ecfbf5870ba06a7b8f6b
brunoslcarvalho <brunoslcarvalho@gmail.com>
2024-09-11 00:31:13 -0300

    V0.2 - Novo Modulo
    Criei um módulo ampliado para o computador, permitindo ao usuário aumentar ou diminuir a tela do chat.

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*
+    document.addEventListener('DOMContentLoaded', function() {
     const modalFoto = document.querySelector('.modalFoto');
     const modalJanela = document.querySelector('.modalJanela');
     const modalBalao = document.querySelector('.modalBalao');
-    const modalFechar = document.querySelector('.modalFechar');
+    const iconeFechar = document.querySelector('.iconeFechar');
 
     // Começa com o modalBalao visível
     modalBalao.classList.remove('hidden');
@@ -14,8 +15,39 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Fecha o modalJanela e exibe o modalBalao ao clicar no botão fechar
-    modalFechar.addEventListener('click', function() {
+    iconeFechar.addEventListener('click', function() {
         modalJanela.classList.add('hidden');
         modalBalao.classList.remove('hidden');
+    });
+});
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modalFoto = document.querySelector('.modalFoto');
+    const modalJanela = document.querySelector('.modalJanela');
+    const modalBalao = document.querySelector('.modalBalao');
+    const iconeFechar = document.querySelector('.iconeFechar');
+    const iconeAumentar = document.querySelector('.iconeAumentar');
+
+    // Começa com o modalBalao visível
+    modalBalao.classList.remove('hidden');
+
+    // Alterna a visibilidade entre modalJanela e modalBalao ao clicar na foto
+    modalFoto.addEventListener('click', function() {
+        modalJanela.classList.toggle('hidden');
+        modalBalao.classList.toggle('hidden');
+    });
+
+    // Fecha o modalJanela e exibe o modalBalao ao clicar no botão fechar
+    iconeFechar.addEventListener('click', function() {
+        modalJanela.classList.add('hidden');
+        modalBalao.classList.remove('hidden');
+    });
+
+    // Alterna o tamanho e centraliza o modal ao clicar no botão aumentar
+    iconeAumentar.addEventListener('click', function() {
+        if (window.innerWidth >= 769) { // Verifica se a tela é maior que 769px
+            modalJanela.classList.toggle('telaCheia');
+        }
     });
 });
