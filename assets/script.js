194c49790fc4f76a2428e9e099116d44cbdba4c9
brunoslcarvalho <brunoslcarvalho@gmail.com>
2024-09-30 20:06:21 -0300

    V.0.5 Reestruturação dos Blocos
    Tive alguns problemas com a camada de fundo, quando tentei inserir essa aplicação em um website wordpress. Então, decide reestruturar todo o código, para eliminá-la e criar novos blocos. Além disso, adicionei um efeito de troca de formato ao botão de tela cheia, bem como uma animação de pulsar no avatar. Além disso, retornei com o sombreamento e também inseri um gradiente no balão.

--- assets/script.js
+++ assets/script.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 1000); // Duração do FadeOut antes de ocultar
     });
 
-    // Alterna tamanho do modal
+    // Alterna tamanho do modal e troca o ícone
     iconeAumentar.addEventListener('click', function() {
         // Verifica tela maior que 768px
         if (window.innerWidth > 768) {
@@ -37,10 +37,16 @@ document.addEventListener('DOMContentLoaded', function() {
                 modal.style.transition = 'width 1s ease, height 1s ease';
                 modal.style.width = '350px';
                 modal.style.height = '80%';
+                
+                // Volta para o ícone de tela cheia original
+                iconeAumentar.src = './assets/img/icon-telacheia1.png';
             } else {
                 modal.style.transition = 'width 1s ease, height 1s ease';
                 modal.style.width = '97vw';
                 modal.style.height = '95vh';
+                
+                // Troca o ícone para o de minimizado
+                iconeAumentar.src = './assets/img/icon-telacheia2.png';
             }
             isFullScreen = !isFullScreen; // Inverte o estado
         }
